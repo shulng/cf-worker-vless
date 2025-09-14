27e9c6b1ea5949f19ef7f9d918661fea327a0401
水灵 <1668589925@qq.com>
2025-09-14 17:40:51 +0800

    feat(worker): 支持通过URL参数动态设置反代IP
    添加从URL查询参数中获取proxyip参数的功能，当存在该参数时，使用其值作为新的反代IP地址。这使得无需修改代码即可动态调整代理服务器地址。

--- _worker.js
+++ _worker.js
@@ -4,9 +4,13 @@ let 反代IP = "ProxyIP.Vultr.CMLiussss.net";
 export default {
   async fetch(访问请求) {
     const 读取我的请求标头 = 访问请求.headers.get("Upgrade");
+    const url = new URL(访问请求.url);
     if (!读取我的请求标头 || 读取我的请求标头 !== "websocket") {
       return new Response(null, { status: 404 });
     } else if (读取我的请求标头 === "websocket") {
+      if (url.searchParams.has("proxyip")) {
+        反代IP = url.searchParams.get("proxyip");
+      }
       return 升级WS请求(访问请求);
     }
     return new Response(null, { status: 404 });
